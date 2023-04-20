3312d00519d477277594c2eb2438cb1f23cdf664
Goran Gajic <you@example.com>
2023-04-20 20:04:16 +0200

    final,cover bad word

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Chat-App 
-Simulation of chat app, when first player write, his message go on left, end when second player write,
-his message go on right. Also, 
+Simulation of chat app,focus on when first player write, his message go on left, end when second player write, his message go on right. Also, we cover bad words.
--- main.js
+++ main.js
@@ -2,20 +2,30 @@ let textAreas = document.querySelectorAll('textarea');
 let mainArea = document.querySelector('.main-area');
 let currentPlayerName = '';
  let direction = ['start','end'];
- let flexBool = true;
+ let flexBool = true; 
+ let badWords = ["shit","fuck","ballshit","apsolutlyfuck"];
+ 
 
 textAreas.forEach(area => area.addEventListener('keyup',placeMessage));
 
 function placeMessage(e){
-if (e.key === "Enter") {
+if (e.key === "Enter") { //get value from input field
     let message = this.value;
-    let playerName = this.id.split('-')[0];
-    if (playerName !== currentPlayerName) {
+    
+    badWords.forEach(word=>{
+        let gg = new RegExp(word,"gi") //solution for more than one bad word and how to cover 
+        message = message.replace(gg,"****")
+    })
+
+    let playerName = this.id.split('-')[0]; //remove area from player1(2)-area
+    if (playerName !== currentPlayerName) { //this is solution for left and right msg
         flexBool = !flexBool;
         currentPlayerName = playerName
     }
 
-    mainArea.innerHTML += `
+    mainArea.innerHTML += 
+    //create template for msg
+    `
     <div class="row" style = "justify-content: ${direction[+flexBool]}">
         <div class="card">
         <p id="move">${playerName}</p>
@@ -23,7 +33,7 @@ if (e.key === "Enter") {
         </div>
     </div>
     `.trim();
-    mainArea.scrollTop = mainArea.scrollHeight;
+    mainArea.scrollTop = mainArea.scrollHeight; 
     this.value = "";
 }
 }
